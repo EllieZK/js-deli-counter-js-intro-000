9f8c483ec5e772bbaa2110df1ad8387589ca6565
Eliana K <ekirshe@gmail.com>
2018-07-30 19:17:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ function takeANumber(katzDeliLine, name){
 };
 
 function nowServing(katzDeliLine){
-  for (var n = 0; n < katzDeliLine.length; n++) {
-    return 'Currently serving ${name}, ${katzDeliLine[n]}.'
-  };
-      return "There is nobody waiting to be served!"
+  if (katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+      else(katzDeliLine = 0) {return "There is nobody waiting to be served!"
+    };
 };
 
 function currentLine(katzDeliLine){
-  if (katzDeliLine.length > 0){
-  return (`The line is currently: ${name}, ${katzDeliLine.length}.`)
-}
-  elseif (katzDeliLine = 0) {
-    return "The line is currently empty."
-  };
 };
