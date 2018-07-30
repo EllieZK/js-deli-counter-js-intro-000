1ca1e6328718136d5d14560b54a856437fcc9a43
Eliana K <ekirshe@gmail.com>
2018-07-30 18:18:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var number = []
+var name = []
+
+function takeANumber(number, name){
+  return (`Welcome, ${name[n]}. You are ${number[n]} in line.`)
+}
+
+function nowServing(){
+  
+}
+
+function currentLine(){
+  
+}
