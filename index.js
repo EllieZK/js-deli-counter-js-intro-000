b33ef9cc60e02c97ef084e807c946ab2b06fba68
Eliana K <ekirshe@gmail.com>
2018-07-30 18:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var number = []
-var name = []
+var katzDeliLine = [];
+var name = [];
 
 function takeANumber(number, name){
-  return (`Welcome, ${name[n]}. You are ${number[n]} in line.`)
+  return (`Welcome, ${name[n]}. You are ${number[n]} in line.`);
 }
 
-function nowServing(){
+function nowServing(currentLine){
   
 }
 
 function currentLine(){
-  
+
 }
